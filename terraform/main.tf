11caa4e914e8516fdb47d72fcffb87a8dd5ff619
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2025-05-04 13:50:56 +0800

    add aws terraform github action

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-097947612b141c026"
+  subnet_id     = "subnet-07b2fcbf26b576cb1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Batman"
+  }
+}
