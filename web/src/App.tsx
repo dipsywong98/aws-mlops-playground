f27254e00e2d782709dc380e66f8bd00f892d4bb
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2025-05-10 11:52:27 +0800

    web mvp

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
+import CanvasDraw from './CanvasDraw';
+import Inference from './Inference';
 
 export default function App() {
+  const [outputArray, setOutputArray] = useState<number[][]>(Array(28).fill(Array(28).fill(0)));
   return (
     <div>
       <h1>Hello, world!</h1>
-      <p>This is a simple React app.</p>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <div>
+          <CanvasDraw value={outputArray} onUpdate={setOutputArray} />
+        </div>
+        <div>
+          <Inference modelPath="/mnist.onnx" inputArray={outputArray} />
+        </div>
+      </div>
     </div>
   );
 }
